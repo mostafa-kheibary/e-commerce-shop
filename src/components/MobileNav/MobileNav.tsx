dcd6b80b5eb1f6504ff984d66885d7ce4db1d325
mostafa kheibary <mostafa.kheibary@gmail.com>
2022-05-10 18:16:18 +0430

    add nav to mobile menu

--- src/components/MobileNav/MobileNav.tsx
+++ src/components/MobileNav/MobileNav.tsx
@@ -1,6 +1,39 @@
 import { useState } from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
+import { Link } from 'react-router-dom';
+import menuData from '../../data/menuData';
 import './MobileNav.css';
+
+const NavLink: React.FC = () => {
+  return (
+    <ul className='nav-link-mobile'>
+      {menuData.map((link, index) => (
+        <li className='mobile-nav__links' key={index}>
+          {link.submenu ? (
+            <>
+              <Link className='mobile-nav__links-text' to={link.url}>
+                <span>{link.name}</span>
+              </Link>
+              <ul>
+                {link.submenu.map((sublink, subindex) => (
+                  <li className='mobile-nav__links__submenu' key={subindex}>
+                    <Link className='mobile-nav__links-text' to={sublink.url}>
+                      {sublink.name}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </>
+          ) : (
+            <Link className='mobile-nav__links-text' to={link.url}>
+              {link.name}
+            </Link>
+          )}
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 const MobileNav: React.FC = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -13,7 +46,18 @@ const MobileNav: React.FC = () => {
         <GiHamburgerMenu className='mobile-nav__button-icon' />
         <span className='mobile-nav__button-text'>MENU</span>
       </button>
-      <div className={`mobile-nav__menu ${isOpen ? 'open' : ''}`}></div>
+      {/* manu */}
+      <div className={`mobile-nav__menu ${isOpen ? 'open' : ''}`}>
+        <div className='mobile-nav__menu__search-box'>
+          <input
+            className='mobile-nav__menu__search-box__search-input'
+            type=''
+            placeholder='search for product'
+          />
+        </div>
+        <NavLink />
+      </div>
+      {/* overlay */}
       <div
         onClick={hanldeOpenMenu}
         className={`over-lay ${isOpen ? 'visible' : ''}`}
