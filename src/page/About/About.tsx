dd94681f0ad28463021776d0ffe403c4a84428cf
mostafa kheibary <mostafa.kheibary@gmail.com>
2022-07-19 04:48:36 +0430

    add about page

--- src/page/About/About.tsx
+++ src/page/About/About.tsx
@@ -1,5 +1,20 @@
+import { Button } from '../../components';
+import { Container } from '../../Layout';
+import './About.css';
+
 const About: React.FC = () => {
-  return <div>About</div>;
+  return (
+    <Container>
+      <div className='about'>
+        <h2 className='about__title'>E-commerce shop</h2>
+        <h4 className='about__autur'>develope by Mostafa Kheibary</h4>
+        <p className='about__discription'>this is personal project for practice purposes</p>
+        <Button className='about__button secoundry'>
+          <a href='https://github.com/mostafa-kheibary/e-commerce-shop'>Github Page</a>
+        </Button>
+      </div>
+    </Container>
+  );
 };
 
 export default About;
